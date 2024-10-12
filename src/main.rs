a7997694243bfb87ef6742a66587b8471f5e9b3e
viveknathani <viveknathani2402@gmail.com>
2024-10-12 18:43:00 +0530

    feat: a basic repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use mymathboard::repl::Repl;
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("hello world!");
+    let mut repl = Repl::new();
+    loop {
+        let mut input = String::new();
+        print!(">>> ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        let result = repl.process_input(&input);
+        println!("=> {:?}", result);
+    }
 }
--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -1,1 +1,46 @@
+use evalexpr::eval_with_context_mut;
+use evalexpr::HashMapContext;
+use std::error::Error;
 
+#[derive(Debug, Clone)]
+pub struct Repl {
+    context: HashMapContext,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Array {}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+#[derive(Debug)]
+pub enum ReplResult {
+    Empty,
+    Boolean(bool),
+    Number(f64),
+    String(String),
+    Point(Point),
+    List(Array),
+}
+
+impl Repl {
+    pub fn new() -> Self {
+        Repl {
+            context: HashMapContext::new(),
+        }
+    }
+
+    pub fn process_input(&mut self, input: &str) -> Result<ReplResult, Box<dyn Error>> {
+        let evaluation_result = eval_with_context_mut(input, &mut self.context)?;
+        match evaluation_result {
+            evalexpr::Value::Boolean(value) => Ok(ReplResult::Boolean(value)),
+            evalexpr::Value::Int(value) => Ok(ReplResult::Number(value as f64)),
+            evalexpr::Value::Float(value) => Ok(ReplResult::Number(value)),
+            evalexpr::Value::String(value) => Ok(ReplResult::String(value)),
+            _ => Ok(ReplResult::Empty),
+        }
+    }
+}
